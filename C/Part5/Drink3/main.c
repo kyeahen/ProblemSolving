32327a0b9de1860ee66f13ebae0cb7e55e69aa71
kyeahen <bright19961@naver.com>
2019-05-01 17:38:45 +0900

    Mod Drink3, NewYearsGreetingMoney

--- C/Part5/Drink3/main.c
+++ C/Part5/Drink3/main.c
@@ -4,48 +4,46 @@ int fibo(int n) {
     if (n == 0) {
         return 0;
     }
-
+    
     if (n == 1) {
         return 1;
     }
-
+    
     return fibo(n - 1) + fibo(n - 2);
 }
 
 int getDrink(int index) {
-
+    
     if (index == 1) {
         return 2;
     }
-
+    
     return fibo(index + 1);
 }
 
 int getSolution(int n) {
-
+    
     int i = 0;
     int m = 0;
     while (fibo(i) != n) {
-
+        i++;
+        
         if (n == fibo(i)) {
             return getDrink(i);
         }
-
+        
         if (fibo(i) > n) {
             m = fibo(i - 1);
             n = n - m;
             return getDrink(i - 1) + getSolution(n);
         }
-
-        i++;
     }
-
 }
 
 int main() {
     int num;
     scanf("%d", &num);
-
+    
     printf("%d", getSolution(num));
     return 0;
 }
--- C/Part5/NewYearsGreetingMoney/main.c
+++ C/Part5/NewYearsGreetingMoney/main.c
@@ -2,58 +2,42 @@
 #include <stdlib.h>
 
 void pick(int* items, int n, int money, int* bucket, int bucketSize, int k) {
-
+    
     if (k == 0) {
         int sum = 0;
-        int i = 0;
-        int index = 0;
-        static int flag = 0;
-
-
-        for (i = 0; i < bucketSize; i++) {
-            sum += items[bucket[i]];
-
-            if (money == sum) {
-                if (flag != index) {
-                    for (int j = 0; j <= i; j++) {
-                        printf("%d ", items[bucket[j]]);
-                    }
-                    printf("\n");
-
-                    flag = index;
+        for (int i = 0; i < bucketSize; i++) {
+            sum += bucket[i];
+        }
+        
+        if (sum == money) {
+            for (int j = 0; j < bucketSize; j++) {
+                if (bucket[j] != 0) {
+                    printf("%d ", bucket[j]);
                 }
             }
-            index = i;
-
+            printf("\n");
         }
-
         return;
     }
-
+    
     int lastIndex = bucketSize - k - 1;
-    int smallest;
-    if (bucketSize == k) {
-        smallest = 0;
-    } else {
-        smallest = bucket[lastIndex];
+    
+    for (int i = 0; i < n; i++) {
+        if (lastIndex == -1 || bucket[lastIndex] <= items[i]) {
+            bucket[lastIndex + 1] = items[i];
+            pick(items, n, money, bucket, bucketSize, k - 1);
+        }
     }
-
-    for(int item = smallest; item < n; item++) {
-        bucket[lastIndex + 1] = item;
-        pick(items, n, money, bucket, bucketSize, k - 1);
-    }
-
 }
 
-
 int main() {
-    int items[] = {1000, 5000, 10000};
+    int items[] = {0, 1000, 5000, 10000};
     int money;
     scanf("%d", &money);
-
-    int num = money / 1000; //bucket의 사이즈는 모두 1000단위로 채운다해도 충분한
+    
+    int num = money / 1000; 
     int *bucket = (int*)malloc(sizeof(int) * num);
-
-    pick(items, 3, money, bucket, num, num);
+    
+    pick(items, 4, money, bucket, num, num);
     return 0;
 }
