5983d57d9535202add71766879d2a08f85a5f1b7
kyeahen <bright19961@naver.com>
2019-05-03 08:32:53 +0900

    Mod Drink, Carry

--- C/Part1/Carry/main.c
+++ C/Part1/Carry/main.c
@@ -1,48 +1,52 @@
 #include <stdio.h>
 
-int main(void) {
-
+int main() {
     int num1, num2;
     scanf("%d %d", &num1, &num2);
-
-    int count = 0;
-    while(num1 != 0 && num2 != 0) {
-
-        int next1 = num1 % 10;
-        int next2 = num2 % 10;
-        int sum = next1 + next2;
-
+    
+    int carry = 0;
+    int flag = 0; //이전 자릿 수의 Carry 여부 판별 변수
+    
+    while (num1 != 0 && num2 != 0) {
+        int a = num1 % 10;
+        int b = num2 % 10;
+        int sum = a + b;
+        
         if (sum >= 10) {
-            count++;
+            carry++;
+            flag = 1;
+        } else {
+            flag = 0;
         }
-
-        num1 /= 10;
-        num2 /= 10;
+        
+        num1 = num1 / 10;
+        num2 = num2 / 10;
     }
-
+    
     while (num1 != 0) {
-
-        int sum = num1 + 1;
-
-        if (sum >= 10) {
-            count++;
+        int a = num1 % 10;
+        int sum = a + 1;
+        
+        if (sum >= 10 && flag == 1) {
+            carry++;
         }
-
-        num1 /= 10;
+        
+        num1 = num1 / 10;
+        
     }
-
+    
     while (num2 != 0) {
-
-        int sum = num2 + 1;
-
-        if (sum >= 10) {
-            count++;
+        int b = num2 % 10;
+        int sum = b + 1;
+        
+        if (sum >= 10 && flag == 1) {
+            carry++;
         }
-
-        num2 /= 10;
+        
+        num2 = num2 / 10;
+        
     }
-
-    printf("%d", count);
-
+    
+    printf("%d", carry);
     return 0;
 }
--- C/Part2/Drink/main.c
+++ C/Part2/Drink/main.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 
-int fibo(int n) {
-    if (n == 0) {
-        return 0;
-    }
-
-    if (n == 1) {
+int fibo(int num) {
+    if (num <= 1) {
         return 1;
     }
-
-    return fibo(n - 1) + fibo(n - 2);
+    
+    return fibo(num - 2) + fibo(num - 1);
 }
 
-int getDrink(int index) {
-
-    if (index == 1) {
+int getDrink(int num) {
+    if (fibo(num) == 1) {
         return 2;
     }
-
-    return fibo(index + 1);
+    
+    return fibo(num + 1);
 }
 
 int main() {
     int num;
     scanf("%d", &num);
-
+    
     int i = 0;
-    while (fibo(i) != num) {
+    while (num != fibo(i)) {
         i++;
     }
-
+    
     printf("%d", getDrink(i));
     return 0;
 }
